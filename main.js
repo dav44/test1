3eee52b2a793c82bb0b70ed5947d87ec70db0b73
david barashi <dudubar44@gmail.com>
2017-11-13 20:09:03 +0200

    test1

---
+++ main.js
@@ -0,0 +1,9 @@
+module.exports = {
+ add: function(num1, num2 ) {
+     return num1+num2
+ },
+ mult: function(num1 ,num2){
+     return num1*num2
+ },
+ 
+}
---
+++ test.js
@@ -0,0 +1,2 @@
+const test = require("./main.js");
+console.log(test.mult(2, 4));
